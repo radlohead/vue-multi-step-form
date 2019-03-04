4c5b40c2740cb43c2b0d6c3e6741bb7bef540c0e
박민호 <radlohead@naver.com>
2019-03-04 19:50:36 +0900

    vuex counter 테스트

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    Clicked: {{ $store.state.count }} times, count is {{ evenOrOdd }}.
+    <button @click="increment">+</button>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  computed: mapGetters([
+    'evenOrOdd'
+  ]),
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
 }
 </script>
 
