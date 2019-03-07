7f6c72133ea0634cc8fe4dad61e1285b3ad54222
박민호 <radlohead@naver.com>
2019-03-07 13:16:45 +0900

    stepMenu step에 따른 disabled처리

--- src/components/StepMenu.vue
+++ src/components/StepMenu.vue
@@ -1,8 +1,19 @@
 <template>
   <section>
-    <button name='back' @click="handleBack">Back</button>
-    <button name='next' @click="handleNext">Next</button>
-    <button name='restart'>Restart</button>
+    <button
+      name='back'
+      :disabled="$store.state.step === 1 || $store.state.step === 5"
+      @click="handleBack"
+    >Back</button>
+    <button
+      name='next'
+      :disabled="$store.state.step === 5"
+      @click="handleNext"
+    >Next</button>
+    <button
+      name='restart'
+      :disabled="$store.state.step !== 5"
+    >Restart</button>
     <div>step: {{ $store.state.step }}</div>
   </section>
 </template>
