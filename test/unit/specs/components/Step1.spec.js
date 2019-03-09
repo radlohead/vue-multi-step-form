8f332825ea51189bf785679e21505cfbaf8d72b0
박민호 <radlohead@naver.com>
2019-03-09 15:40:22 +0900

    Step1 테스트 코드 작성

--- test/unit/specs/components/Step1.spec.js
+++ test/unit/specs/components/Step1.spec.js
@@ -9,37 +9,25 @@
 //   })
 // })
 
-import Vue from 'vue'
 import Vuex from 'vuex'
-import { mount, createLocalVue } from '@vue/test-utils'
+import { shallowMount, createLocalVue,  } from '@vue/test-utils'
 import Step1 from '@/components/Step1'
 
-// const localVue = createLocalVue()
-// localVue.use(Vuex)
+const localVue = createLocalVue()
+let store
+localVue.use(Vuex)
 
-// const storeOptions = {}
-// const mockStore = new Vuex.Store(storeOptions)
+describe('Step1 describe', () => {
+  let wrapper
 
-// describe('Step1 mock', () => {
-//   let wrapper
-//   beforeEach(() => {
-//     const mountOptions = {
-//       localVue,
-//       store: mockStore
-//     }
-//     wrapper = mount(Step1, mountOptions)
-//   })
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {}
+    })
+  })
   
-//   test('Step1 test', () => {
-//     expect(wrapper.isVueInstance()).toBeTruthy()
-//   })
-// })
-
-
-describe('Step1.vue', () => {
   it('Step1 test', () => {
-    const Constructor = Vue.extend(Step1)
-    const vm = new Constructor().$mount()
-    expect(vm.updateCheckbox()).toEqual(1)
+    wrapper = shallowMount(Step1, { store, localVue })
+    expect(wrapper.isVueInstance()).toBeTruthy()
   })
 })
