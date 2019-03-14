190e7b1900b367f074d195494c3c745be5230730
박민호 <radlohead@naver.com>
2019-03-14 16:00:56 +0900

    contants 테스트 코드 수정

--- test/unit/specs/store/constants.spec.js
+++ test/unit/specs/store/constants.spec.js
@@ -13,76 +13,110 @@ import {
 } from '@/store/constants';
 
 describe('constants formType test', () => {
+    const mockFormType = {
+        CHECKBOX: 'CHECKBOX',
+        RADIO: 'RADIO',
+        TEXT: 'TEXT',
+        SELECTBOX: 'SELECTBOX'
+    }
     it('CHECKBOX', () => {
-        expect(CHECKBOX).toBe('CHECKBOX');
+        expect(CHECKBOX).toBe(mockFormType[CHECKBOX]);
     });
     it('RADIO', () => {
-        expect(RADIO).toBe('RADIO');
+        expect(RADIO).toBe(mockFormType[RADIO]);
     });
     it('TEXT', () => {
-        expect(TEXT).toBe('TEXT');
+        expect(TEXT).toBe(mockFormType[TEXT]);
     });
     it('SELECTBOX', () => {
-        expect(SELECTBOX).toBe('SELECTBOX');
+        expect(SELECTBOX).toBe(mockFormType[SELECTBOX]);
     });
 });
 
 describe('constants formType number test', () => {
+    const mockFormTypeIndex = {
+        CHECKBOX: 1,
+        RADIO: 2,
+        TEXT: 3,
+        SELECTBOX: 4
+    }
+
     it('CHECKBOX number', () => {
-        expect(FORM_TYPE_NAME[CHECKBOX]).toBe(1);
+        expect(FORM_TYPE_NAME[CHECKBOX]).toBe(mockFormTypeIndex[CHECKBOX]);
     });
     it('RADIO number', () => {
-        expect(FORM_TYPE_NAME[RADIO]).toBe(2);
+        expect(FORM_TYPE_NAME[RADIO]).toBe(mockFormTypeIndex[RADIO]);
     });
     it('TEXT number', () => {
-        expect(FORM_TYPE_NAME[TEXT]).toBe(3);
+        expect(FORM_TYPE_NAME[TEXT]).toBe(mockFormTypeIndex[TEXT]);
     });
     it('SELECTBOX number', () => {
-        expect(FORM_TYPE_NAME[SELECTBOX]).toBe(4);
+        expect(FORM_TYPE_NAME[SELECTBOX]).toBe(mockFormTypeIndex[SELECTBOX]);
     });
 });
 
 describe('constants STEP1 ~ 4 test', () => {
+    const mockSteps = {
+        STEP1: 'STEP1',
+        STEP2: 'STEP2',
+        STEP3: 'STEP3',
+        STEP4: 'STEP4'
+    }
+
     it('STEP1', () => {
-        expect(STEP1).toBe('STEP1');
+        expect(STEP1).toBe(mockSteps[STEP1]);
     });
     it('STEP2', () => {
-        expect(STEP2).toBe('STEP2');
+        expect(STEP2).toBe(mockSteps[STEP2]);
     });
     it('STEP3', () => {
-        expect(STEP3).toBe('STEP3');
+        expect(STEP3).toBe(mockSteps[STEP3]);
     });
     it('STEP4', () => {
-        expect(STEP4).toBe('STEP4');
+        expect(STEP4).toBe(mockSteps[STEP4]);
     });
 });
 
 describe('constants FORM_ITEM_ID test', () => {
+    const mockFormTypeId = {
+        STEP1: 1,
+        STEP2: 2,
+        STEP3: 3,
+        STEP4: 4
+    }
+
     it('STEP1', () => {
-        expect(FORM_ITEM_ID[STEP1]).toBe(1);
+        expect(FORM_ITEM_ID[STEP1]).toBe(mockFormTypeId[STEP1]);
     });
     it('STEP2', () => {
-        expect(FORM_ITEM_ID[STEP2]).toBe(2);
+        expect(FORM_ITEM_ID[STEP2]).toBe(mockFormTypeId[STEP2]);
     });
     it('STEP3', () => {
-        expect(FORM_ITEM_ID[STEP3]).toBe(3);
+        expect(FORM_ITEM_ID[STEP3]).toBe(mockFormTypeId[STEP3]);
     });
     it('STEP4', () => {
-        expect(FORM_ITEM_ID[STEP4]).toBe(4);
+        expect(FORM_ITEM_ID[STEP4]).toBe(mockFormTypeId[STEP4]);
     });
 });
 
 describe('stepExceptionAlertMessage test', () => {
-    if('checkbox alert message', () => {
+    const mockStepExceptionAlertMessage = {
+        CHECKBOX: '청소 스타일을 체크해 주세요.',
+        RADIO: '청소 시간을 체크해 주세요.',
+        TEXT: '원하는 청소 스타일을 추가로 입력해 주세요.',
+        SELECTBOX: '네번째 질문을 선택해 주세요.'
+    }
+    
+    it('checkbox alert message', () => {
         expect(stepExceptionAlertMessage[CHECKBOX]).toBe('청소 스타일을 체크해 주세요.');
     });
-    if('radio alert message', () => {
-        expect(stepExceptionAlertMessage[CHECKBOX]).toBe('청소 시간을 체크해 주세요.');
+    it('radio alert message', () => {
+        expect(stepExceptionAlertMessage[RADIO]).toBe('청소 시간을 체크해 주세요.');
     });
-    if('text alert message', () => {
-        expect(stepExceptionAlertMessage[CHECKBOX]).toBe('원하는 청소 스타일을 추가로 입력해 주세요.');
+    it('text alert message', () => {
+        expect(stepExceptionAlertMessage[TEXT]).toBe('원하는 청소 스타일을 추가로 입력해 주세요.');
     });
-    if('selectbox alert message', () => {
-        expect(stepExceptionAlertMessage[CHECKBOX]).toBe('네번째 질문을 선택해 주세요.');
+    it('selectbox alert message', () => {
+        expect(stepExceptionAlertMessage[SELECTBOX]).toBe('네번째 질문을 선택해 주세요.');
     });
 });
