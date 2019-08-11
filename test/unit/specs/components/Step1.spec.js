96accae5f0c845709f9721f9ce5c7d026b548972
= <radlohead@naver.com>
2019-08-11 14:00:55 +0900

    젠킨스 오랜만에 테스트3

--- test/unit/specs/components/Step1.spec.js
+++ test/unit/specs/components/Step1.spec.js
@@ -1,24 +1,24 @@
-import Vuex from 'vuex';
-import { shallowMount, createLocalVue, } from '@vue/test-utils';
-import Step1 from '@/components/Step1';
+import Vuex from 'vuex'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import Step1 from '@/components/Step1'
 
-const localVue = createLocalVue();
-localVue.use(Vuex);
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Step1 describe', () => {
-	let store;
+  let store
 
-	beforeEach(() => {
-		store = new Vuex.Store({
-			state: {},
-			getters: {
-				step1Title(){}
-			}
-		})
-	});
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {},
+      getters: {
+        step1Title() {}
+      }
+    })
+  })
 
-	it('Step1 test', () => {
-		const wrapper = shallowMount(Step1, { store, localVue });
-		expect(wrapper.isVueInstance()).toBeTruthy();
-	});
-});
+  it('Step1 test', () => {
+    const wrapper = shallowMount(Step1, { store, localVue })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
